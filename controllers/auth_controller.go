1651fa6bc7d86f40716823a8bc977bffa10627e1
yolopyang <yolopyang@tencent.com>
2024-10-20 20:03:16 +0800

    用户信息入库，完成注册

--- controllers/auth_controller.go
+++ controllers/auth_controller.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"exchangeapp/global"
 	"exchangeapp/utils"
 	"github.com/gin-gonic/gin"
 	"net/http"
@@ -26,5 +27,17 @@ func Register(ctx *gin.Context) {
 		return
 	}
 	// 完成 加密，token 生成
+
+	if err := global.DB.AutoMigrate(&user); err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	// 将用户插入数据库
+	if err := global.DB.Create(&user).Error; err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
 	ctx.JSON(http.StatusOK, gin.H{"token": jwt})
 }
