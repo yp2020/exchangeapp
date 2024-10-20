462a07e67dc22bf6f049f08dbd0f0707871533ca
yolopyang <yolopyang@tencent.com>
2024-10-20 20:46:49 +0800

    创建 汇率接口

---
+++ controllers/exchange_rate_controller.go
@@ -0,0 +1,41 @@
+package controllers
+
+import (
+	"exchangeapp/global"
+	"exchangeapp/models"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"time"
+)
+
+// CreateExchangeRate 创建汇率信息
+func CreateExchangeRate(ctx *gin.Context) {
+	var exchangeRate models.ExchangeRate
+	if err := ctx.ShouldBindJSON(&exchangeRate); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	exchangeRate.Date = time.Now()
+	// 自动迁移，建立表
+	if err := global.DB.AutoMigrate(&exchangeRate); err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	// 将数据插入数据库
+	if err := global.DB.Create(&exchangeRate).Error; err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	ctx.JSON(http.StatusOK, gin.H{"data": exchangeRate})
+}
+
+// GetExchangeRate 获取汇率数据
+func GetExchangeRate(ctx *gin.Context) {
+	var exchangeRates []models.ExchangeRate
+	if err := global.DB.Find(&exchangeRates).Error; err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	ctx.JSON(http.StatusOK, gin.H{"data": exchangeRates})
+}
